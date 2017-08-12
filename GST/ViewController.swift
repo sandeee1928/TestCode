1c7de5e4a94dc1198a61dd4d57135fff572ff0ab
Rubiya <rubiya.faniband@honeywell.com>
2017-08-12 16:16:53 +0530

    Initial Commit

---
+++ GST/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GST
+//
+//  Created by Rubiya on 12/08/17.
+//  Copyright © 2017 Rubiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
